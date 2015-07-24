1de9e77467815d9cf2f8ef1043f6d2cd91114f1f
Ryo Kanbayashi <ryo@User-no-MacBook-Air.local>
2015-07-24 20:49:08 +0900

    fx sample succeeded

---
+++ pyalog_fx.py
@@ -0,0 +1,77 @@
+from pyalgotrade import strategy
+from pyalgotrade import plotter
+from pyalgotrade.tools import yahoofinance, quandl
+from pyalgotrade.technical import bollinger
+from pyalgotrade.stratanalyzer import sharpe
+from pyalgotrade.barfeed import yahoofeed
+import pandas.io.data as web
+import pytz
+from datetime import datetime
+
+
+class BBands(strategy.BacktestingStrategy):
+    def __init__(self, feed, instrument, bBandsPeriod):
+        strategy.BacktestingStrategy.__init__(self, feed)
+        self.__instrument = instrument
+        self.__bbands = bollinger.BollingerBands(feed[instrument].getCloseDataSeries(), bBandsPeriod, 2)
+
+    def getBollingerBands(self):
+        return self.__bbands
+
+    def onBars(self, bars):
+        lower = self.__bbands.getLowerBand()[-1]
+        upper = self.__bbands.getUpperBand()[-1]
+        if lower is None:
+            return
+
+        shares = self.getBroker().getShares(self.__instrument)
+        bar = bars[self.__instrument]
+        if shares == 0 and bar.getClose() < lower:
+            sharesToBuy = int(self.getBroker().getCash(False) / bar.getClose())
+            self.marketOrder(self.__instrument, sharesToBuy)
+        elif shares > 0 and bar.getClose() > upper:
+            self.marketOrder(self.__instrument, -1*shares)
+
+def main(plot):
+    bBandsPeriod = 40
+
+    # instrument = "yhoo"
+    # # Download the bars.
+    # feed_dummy = yahoofinance.build_feed([instrument], 2010, 2015, ".")
+    # feed = quandl.build_feed("CURRFX", ["USDJPY"], 2010, 2015, ".", noAdjClose=True)
+    start = datetime(2010, 1, 1, 0, 0, 0, 0, pytz.utc)
+    end = datetime(2013, 1, 1, 0, 0, 0, 0, pytz.utc)
+    data = web.DataReader('DEXJPUS', 'fred', start, end)
+
+    frslt = open('./hoge.csv', 'w')
+    frslt.write("Date,Open,High,Low,Close,Volume,Adj Close\n")
+    for k,v in data["DEXJPUS"].iteritems():
+        if v != v: # if non
+            continue
+            
+        frslt.write(str(k) + "," + str(v) + "," + \
+                    str(v) + "," + str(v) + \
+                    "," + str(v) + ",1000000,"+ str(v) + "\n")
+    frslt.close()
+
+    instrument = "USDJPY"
+    feed = yahoofeed.Feed()
+    feed.addBarsFromCSV(instrument, "./hoge.csv")
+    strat = BBands(feed, instrument, bBandsPeriod)
+    sharpeRatioAnalyzer = sharpe.SharpeRatio()
+    strat.attachAnalyzer(sharpeRatioAnalyzer)
+    
+    if plot:
+        plt = plotter.StrategyPlotter(strat, True, True, True)
+        plt.getInstrumentSubplot(instrument).addDataSeries("upper", strat.getBollingerBands().getUpperBand())
+        plt.getInstrumentSubplot(instrument).addDataSeries("middle", strat.getBollingerBands().getMiddleBand())
+        plt.getInstrumentSubplot(instrument).addDataSeries("lower", strat.getBollingerBands().getLowerBand())
+
+    strat.run()
+    print "Sharpe ratio: %.2f" % sharpeRatioAnalyzer.getSharpeRatio(0.05)
+
+    if plot:
+        plt.plot()
+
+if __name__ == "__main__":
+    main(True)
