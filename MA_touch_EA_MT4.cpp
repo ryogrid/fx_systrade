0f1cad107b6cf00771693c21a45abfccd5f224c4
Ryo Kanbayashi <ryo@User-no-MacBook-Air.local>
2015-08-10 07:48:52 +0900

    draft of simple ma mq4 code finished

--- MA_touch_EA_MT4.cpp
+++ MA_touch_EA_MT4.cpp
@@ -1,126 +1,148 @@
-//+------------------------------------------------------------------+
-//|                                               Moving Average.mq4 |
-//|                      Copyright © 2005, MetaQuotes Software Corp. |
-//|                                       http://www.metaquotes.net/ |
-//+------------------------------------------------------------------+
-#define MAGICMA  20050610
+#define MAGICMA  20150810
 
-extern double Lots               = 0.1;
-extern double MaximumRisk        = 0.02;
-extern double DecreaseFactor     = 3;
-extern double MovingPeriod       = 12;
-extern double MovingShift        = 6;
-//+------------------------------------------------------------------+
-//| Calculate open positions                                         |
-//+------------------------------------------------------------------+
-int CalculateCurrentOrders(string symbol)
-  {
-   int buys=0,sells=0;
-//----
-   for(int i=0;i<OrdersTotal();i++)
-     {
-      if(OrderSelect(i,SELECT_BY_POS,MODE_TRADES)==false) break;
-      if(OrderSymbol()==Symbol() && OrderMagicNumber()==MAGICMA)
-        {
-         if(OrderType()==OP_BUY)  buys++;
-         if(OrderType()==OP_SELL) sells++;
-        }
-     }
-//---- return orders volume
-   if(buys>0) return(buys);
-   else       return(-sells);
+#define TRADE_TYPE_BUY 1
+#define TRADE_TYPE_SELL 2
+#define TRADE_TYPE_NOT_HAVE 3
+
+#define MA_PERIOD = 20
+
+#define FALSE 0
+#define TRUE 1
+
+
+int current_trade = TRADE_TYPE_NOT_HAVE;
+
+int touch_top(double ma_cur, double ma_prev1, double ma_prev2)
+{
+  if (Close[2] < ma_prev1 && Close[1] > ma_prev2 && Close[0] < ma_cur){
+    return TRUE;
+  } else {
+    return FALSE;
   }
-//+------------------------------------------------------------------+
-//| Calculate optimal lot size                                       |
-//+------------------------------------------------------------------+
-double LotsOptimized()
-  {
-   double lot=Lots;
-   int    orders=HistoryTotal();     // history orders total
-   int    losses=0;                  // number of losses orders without a break
-//---- select lot size
-   lot=NormalizeDouble(AccountFreeMargin()*MaximumRisk/1000.0,1);
-//---- calcuulate number of losses orders without a break
-   if(DecreaseFactor>0)
-     {
-      for(int i=orders-1;i>=0;i--)
-        {
-         if(OrderSelect(i,SELECT_BY_POS,MODE_HISTORY)==false) { Print("Error in history!"); break; }
-         if(OrderSymbol()!=Symbol() || OrderType()>OP_SELL) continue;
-         //----
-         if(OrderProfit()>0) break;
-         if(OrderProfit()<0) losses++;
-        }
-      if(losses>1) lot=NormalizeDouble(lot-lot*losses/DecreaseFactor,1);
-     }
-//---- return lot size
-   if(lot<0.1) lot=0.1;
-   return(lot);
+}
+
+int touch_bottom(double ma_cur, double ma_prev1, double ma_prev2)
+{
+  if (Close[2] > ma_prev1 && Close[1] < ma_prev2 && Close[0] > ma_cur){
+    return TRUE;
+  } else {
+    return FALSE;
   }
+}
+
+int cross_above(double ma_cur, double ma_prev1)
+{
+  if (Close[1] < ma_prev1 && Close[0] > ma_cur){
+    return TRUE;
+  } else {
+    return FALSE;
+  }  
+}
+
+int cross_below(double ma_cur, double ma_prev1)
+{
+  if (Close[1] > ma_prev1 && Close[0] < ma_cur){
+    return TRUE;
+  } else {
+    return FALSE;
+  }  
+}
+
 //+------------------------------------------------------------------+
 //| Check for open order conditions                                  |
 //+------------------------------------------------------------------+
 void CheckForOpen()
-  {
-   double ma;
-   int    res;
+{
+   double ma_cur;
+   double ma_prev1;
+   double ma_prev2;
+   
 //---- go trading only for first tiks of new bar
-   if(Volume[0]>1) return;
+   if(Volume[0] > 1){
+     return;
+   }
 //---- get Moving Average 
-   ma=iMA(NULL,0,MovingPeriod,MovingShift,MODE_SMA,PRICE_CLOSE,0);
-//---- sell conditions
-   if(Open[1]>ma && Close[1]<ma)  
-     {
-      res=OrderSend(Symbol(),OP_SELL,LotsOptimized(),Bid,3,0,0,"",MAGICMA,0,Red);
-      return;
-     }
+   ma_cur = iMA(NULL, 0, MA_PERIOD, 0, MODE_SMA, PRICE_CLOSE, 0);
+   ma_prev1 = iMA(NULL, 0, MA_PERIOD, 0, MODE_SMA, PRICE_CLOSE, 1);
+   ma_prev2 = iMA(NULL, 0, MA_PERIOD, 0, MODE_SMA, PRICE_CLOSE, 2);
+   
 //---- buy conditions
-   if(Open[1]<ma && Close[1]>ma)  
-     {
-      res=OrderSend(Symbol(),OP_BUY,LotsOptimized(),Ask,3,0,0,"",MAGICMA,0,Blue);
-      return;
-     }
+   if(touch_bottom(ma_cur, ma_prev1, ma_prev2)) {
+     OrderSend(Symbol(), OP_SELL, NormalizeDouble(AccountFreeMargin()/1000.0,1), Bid, 3, 0, 0, "", MAGICMA, 0, Blue);
+     current_trade = TRADE_TYPE_SELL;
+     return;
+   }
+//---- sell conditions
+   if(touch_top(ma_cur, ma_prev1, ma_prev2)) {
+     OrderSend(Symbol(), OP_BUY, NormalizeDouble(AccountFreeMargin()/1000.0,1), Ask, 3, 0, 0, "", MAGICMA, 0, Red);
+     current_trade = TRADE_TYPE_BUY;     
+     return;
+   }
+
 //----
-  }
+}
 //+------------------------------------------------------------------+
 //| Check for close order conditions                                 |
 //+------------------------------------------------------------------+
 void CheckForClose()
-  {
-   double ma;
+{
+   double ma_cur;
+   double ma_prev1;
+   double ma_prev2;
+   
 //---- go trading only for first tiks of new bar
-   if(Volume[0]>1) return;
-//---- get Moving Average 
-   ma=iMA(NULL,0,MovingPeriod,MovingShift,MODE_SMA,PRICE_CLOSE,0);
+   if(Volume[0] > 1) {
+     return;
+   }
+//---- get Moving Average
+   ma_cur = iMA(NULL, 0, MA_PERIOD, 0, MODE_SMA, PRICE_CLOSE, 0);
+   ma_prev1 = iMA(NULL, 0, MA_PERIOD, 0, MODE_SMA, PRICE_CLOSE, 1);
+   ma_prev2 = iMA(NULL, 0, MA_PERIOD, 0, MODE_SMA, PRICE_CLOSE, 2);   
 //----
-   for(int i=0;i<OrdersTotal();i++)
+   for(int i = 0;i < OrdersTotal();i++)
      {
-      if(OrderSelect(i,SELECT_BY_POS,MODE_TRADES)==false)        break;
-      if(OrderMagicNumber()!=MAGICMA || OrderSymbol()!=Symbol()) continue;
-      //---- check order type 
-      if(OrderType()==OP_BUY)
-        {
-         if(Open[1]>ma && Close[1]<ma) OrderClose(OrderTicket(),OrderLots(),Bid,3,White);
-         break;
+       if(OrderSelect(i, SELECT_BY_POS, MODE_TRADES) == false){
+	 break;
+       }
+       if(OrderMagicNumber() != MAGICMA || OrderSymbol() != Symbol()){
+	 continue;
+       }
+       //---- check order type 
+       if(OrderType() == OP_BUY)
+	 {
+	   if(cross_below(ma_cur, ma_prev1) || touch_bottom(ma_cur, ma_prev1, ma_prev2)){
+	     OrderClose(OrderTicket(), OrderLots(), Bid, 3, White);
+	   }
+	   current_trade = TRADE_TYPE_NOT_HAVE;
+	   break;
         }
-      if(OrderType()==OP_SELL)
+      if(OrderType() == OP_SELL)
         {
-         if(Open[1]<ma && Close[1]>ma) OrderClose(OrderTicket(),OrderLots(),Ask,3,White);
-         break;
+	  if(cross_above(ma_cur, ma_prev1) || touch_top(ma_cur, ma_prev1, ma_prev2)){
+	    OrderClose(OrderTicket(), OrderLots(), Ask, 3, White);
+	  }
+	  current_trade = TRADE_TYPE_NOT_HAVE;	  
+	  break;
         }
      }
 //----
-  }
+}
 //+------------------------------------------------------------------+
 //| Start function                                                   |
 //+------------------------------------------------------------------+
 void start()
-  {
+{
 //---- check for history and trading
-   if(Bars<100 || IsTradeAllowed()==false) return;
+    if (Bars < 100 || IsTradeAllowed() == false){
+      return;
+    }
 //---- calculate open orders by current symbol
-   if(CalculateCurrentOrders(Symbol())==0) CheckForOpen();
-   else                                    CheckForClose();
+
+    if (current_trade == TRADE_TYPE_NOT_HAVE){
+      CheckForOpen();
+    } else {
+      CheckForClose();
+    }
 //----
-  }
+}
 //+------------------------------------------------------------------+
