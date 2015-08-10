87982ab560aefe160065aca840d9a71a3ae6c094
Ryo Kanbayashi <ryo@User-no-MacBook-Air.local>
2015-08-10 15:38:48 +0900

    EA tested at MT4 but not good

--- MA_touch_EA_MT4.cpp
+++ MA_touch_EA_MT4.cpp
@@ -4,13 +4,10 @@
 #define TRADE_TYPE_SELL 2
 #define TRADE_TYPE_NOT_HAVE 3
 
-#define MA_PERIOD = 20
+#define MA_PERIOD 20
 
 #define FALSE 0
 #define TRUE 1
-
-
-int current_trade = TRADE_TYPE_NOT_HAVE;
 
 int touch_top(double ma_cur, double ma_prev1, double ma_prev2)
 {
@@ -48,6 +45,20 @@ int cross_below(double ma_cur, double ma_prev1)
   }  
 }
 
+int check_active_order()
+{
+  for(int i = 0;i < OrdersTotal();i++) {
+    if(OrderSelect(i, SELECT_BY_POS, MODE_TRADES) == false){
+      break;
+    }
+    if(OrderMagicNumber() != MAGICMA || OrderSymbol() != Symbol()){
+      continue;
+    }
+    return TRUE;
+  }
+  return FALSE;
+}
+
 //+------------------------------------------------------------------+
 //| Check for open order conditions                                  |
 //+------------------------------------------------------------------+
@@ -68,14 +79,12 @@ void CheckForOpen()
    
 //---- buy conditions
    if(touch_bottom(ma_cur, ma_prev1, ma_prev2)) {
-     OrderSend(Symbol(), OP_SELL, NormalizeDouble(AccountFreeMargin()/1000.0,1), Bid, 3, 0, 0, "", MAGICMA, 0, Blue);
-     current_trade = TRADE_TYPE_SELL;
+     OrderSend(Symbol(), OP_SELL, NormalizeDouble(AccountFreeMargin()*0.1/1000.0,1), Bid, 3, 0, 0, "", MAGICMA, 0, Blue);
      return;
    }
 //---- sell conditions
    if(touch_top(ma_cur, ma_prev1, ma_prev2)) {
-     OrderSend(Symbol(), OP_BUY, NormalizeDouble(AccountFreeMargin()/1000.0,1), Ask, 3, 0, 0, "", MAGICMA, 0, Red);
-     current_trade = TRADE_TYPE_BUY;     
+     OrderSend(Symbol(), OP_BUY, NormalizeDouble(AccountFreeMargin()*0.1/1000.0,1), Ask, 3, 0, 0, "", MAGICMA, 0, Red); 
      return;
    }
 
@@ -102,7 +111,7 @@ void CheckForClose()
    for(int i = 0;i < OrdersTotal();i++)
      {
        if(OrderSelect(i, SELECT_BY_POS, MODE_TRADES) == false){
-	 break;
+	      break;
        }
        if(OrderMagicNumber() != MAGICMA || OrderSymbol() != Symbol()){
 	 continue;
@@ -110,18 +119,16 @@ void CheckForClose()
        //---- check order type 
        if(OrderType() == OP_BUY)
 	 {
-	   if(cross_below(ma_cur, ma_prev1) || touch_bottom(ma_cur, ma_prev1, ma_prev2)){
+	   if(cross_below(ma_cur, ma_prev1) || touch_bottom(ma_cur, ma_prev1, ma_prev2) || OrderProfit() > AccountFreeMargin()*(-0.07)){
 	     OrderClose(OrderTicket(), OrderLots(), Bid, 3, White);
 	   }
-	   current_trade = TRADE_TYPE_NOT_HAVE;
 	   break;
         }
       if(OrderType() == OP_SELL)
         {
-	  if(cross_above(ma_cur, ma_prev1) || touch_top(ma_cur, ma_prev1, ma_prev2)){
+	  if(cross_above(ma_cur, ma_prev1) || touch_top(ma_cur, ma_prev1, ma_prev2)|| OrderProfit() > AccountFreeMargin()*(-0.07)){
 	    OrderClose(OrderTicket(), OrderLots(), Ask, 3, White);
-	  }
-	  current_trade = TRADE_TYPE_NOT_HAVE;	  
+	  }  
 	  break;
         }
      }
@@ -138,7 +145,7 @@ void start()
     }
 //---- calculate open orders by current symbol
 
-    if (current_trade == TRADE_TYPE_NOT_HAVE){
+    if (check_active_order() == FALSE){
       CheckForOpen();
     } else {
       CheckForClose();
