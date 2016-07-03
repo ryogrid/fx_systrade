cf9655485b40ed562ba28b060310d7912946ba0d
Ryo Kanbayashi <ryo.contact@gmail.com>
2016-07-03 10:05:32 +0900

    add lods tune logic

--- trap_repat_trade.py
+++ trap_repat_trade.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python
+from math import log
+
+def get_tuned_percent(baseline_price):
+    return log(150-baseline_price)
+
+def get_baseline_lots(portfolio, cur_price):
+    return portfolio / cur_price / 50
     
 """
 main
@@ -20,17 +27,19 @@ print "data size: " + str(data_len)
 
 
 money = 5000000
-HALF_SPREAD = 0.015 #0.015
+portfolio = money
+HALF_SPREAD = 0.015
 
-BUY_LOTS = 1000
+BUY_LOTS = 500
 
-WON_PIPS = 0.3
-SONKIRI_PIPS = 0.1
+WON_PIPS = 0.1
+SONKIRI_PIPS = 0.5
 
-center_val = int(exchange_rates[0])
+center_val = float(int(exchange_rates[0]))
 traps = []
-for price in xrange(center_val-50, center_val+50):
-    traps.append([price, False, False])
+for price in xrange(500):
+    traps.append([center_val - price/10.0 , False, False, 5, 0])
+    traps.append([center_val + price/10.0 , False, False, 5, 0])
 
 prev_positions = 0
 cur_positions = 0
@@ -45,14 +54,17 @@ for cur in xrange(1, data_len):
                and traps[idx][0] <= (exchange_rates[cur]+HALF_SPREAD) \
                and traps[idx][1] == False:
                 traps[idx][1] = True
-                money -= exchange_rates[cur] * BUY_LOTS
+                traps[idx][4] = get_baseline_lots(portfolio, exchange_rates[cur]) * get_tuned_percent(exchange_rates[cur])
+                money -= exchange_rates[cur] * get_baseline_lots(portfolio, exchange_rates[cur]) * get_tuned_percent(exchange_rates[cur])
+                
     else:
         for idx in xrange(len(traps)):
             if traps[idx][0] < (exchange_rates[cur-1]+HALF_SPREAD) \
                and traps[idx][0] >= (exchange_rates[cur]+HALF_SPREAD) \
                and traps[idx][1] == False:
                 traps[idx][1] = True
-                money -= exchange_rates[cur] * BUY_LOTS        
+                traps[idx][4] = get_baseline_lots(portfolio, exchange_rates[cur]) * get_tuned_percent(exchange_rates[cur])                
+                money -= exchange_rates[cur] * get_baseline_lots(portfolio, exchange_rates[cur]) * get_tuned_percent(exchange_rates[cur])
 
     # close position
     for idx in xrange(len(traps)):
@@ -68,21 +80,24 @@ for cur in xrange(1, data_len):
                     # traps[idx][2] = False
                     # is_print = True
             elif traps[idx][2] == True and (exchange_rates[cur]-HALF_SPREAD) - traps[idx][0] < SONKIRI_PIPS:
-                money += (exchange_rates[cur]-HALF_SPREAD) * BUY_LOTS
+                money += (exchange_rates[cur]-HALF_SPREAD) * traps[idx][4]
                 traps[idx][1] = False
                 traps[idx][2] = False
                 is_print = True
            
-    # count positions
-    cur_positions = 0
-    for idx in xrange(len(traps)):
-        if traps[idx][1] == True:
-            cur_positions += 1
+    # # count positions
+    # cur_positions = 0
+    # for idx in xrange(len(traps)):
+    #     if traps[idx][1] == True:
+    #         cur_positions += 1
     
             
     # calculate portfolio
     if is_print == True:
-        portfolio = (exchange_rates[cur] * BUY_LOTS * cur_positions) + money
+        portfolio = money
+        for idx in xrange(len(traps)):
+            if traps[idx][1] == True:
+                portfolio += (exchange_rates[cur] * traps[idx][4])
         print exchange_dates[cur] + " " + str(portfolio)
 
 
