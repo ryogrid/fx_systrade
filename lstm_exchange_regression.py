c39f415f5cfba904c03d37ec459400967179c1fd
Ryo Kanbayashi <ryo.contact@gmail.com>
2017-03-04 08:16:36 +0900

    rewrite lstm_exchange_regression.py

--- lstm_exchange_regression.py
+++ lstm_exchange_regression.py
@@ -1,17 +1,19 @@
-import numpy as np
-import pandas as pd
 import tensorflow as tf
-from tensorflow.python.framework import dtypes
-from tensorflow.contrib import learn as tflearn
-from tensorflow.contrib import layers as tflayers
+import numpy as np
+import random
 
-from sklearn.metrics import mean_squared_error
+num_of_input_nodes = 1
+num_of_hidden_nodes = 80
+num_of_output_nodes = 1
+length_of_sequences = 10
+num_of_training_epochs = 5000
+size_of_mini_batch = 100
+num_of_prediction_epochs = 100
+learning_rate = 0.01
+forget_bias = 0.8
+num_of_sample = 1000
 
-from matplotlib import pyplot as plt
-
-#################################################
 rates_fd = open('./hoge.csv', 'r')
-
 exchange_rates = []
 for line in rates_fd:
     splited = line.split(",")
@@ -19,146 +21,136 @@ for line in rates_fd:
         time = splited[0].replace("/", "-") + " " + splited[1]
         val = float(splited[2]) #for hoge.csv
         exchange_rates.append(val)
-#################################################
+        
+def get_batch(batch_size, X, t):
+    rnum = [random.randint(0, len(X) - 1) for x in range(batch_size)]
+    xs = np.array([[[y] for y in list(X[r])] for r in rnum])
+    ts = np.array([[t[r]] for r in rnum])
+    return xs, ts
 
-def exchange_rate():
-    return exchange_rates[:100000]
+def create_data(nb_of_samples, sequence_len):
+    X = np.zeros((nb_of_samples, sequence_len))
+    t = np.zeros((nb_of_samples, 1))
+    for row_idx in range(nb_of_samples):
+        X[row_idx, :] = exchange_rates[row_idx:row_idx+sequence_len]
+        t[row_idx, :] = exchange_rates[row_idx+sequence_len+1]
 
-def rnn_data(data, time_steps, labels=False):
-    """
-    creates new data frame based on previous observation
-      * example:
-        l = [1, 2, 3, 4, 5]
-        time_steps = 2
-        -> labels == False [[1, 2], [2, 3], [3, 4]]
-        -> labels == True [3, 4, 5]
-    """
-    rnn_df = []
-    for i in range(len(data) - time_steps):
-        if labels:
-            try:
-                rnn_df.append(data.iloc[i + time_steps].as_matrix())
-            except AttributeError:
-                rnn_df.append(data.iloc[i + time_steps])
-        else:
-            data_ = data.iloc[i: i + time_steps].as_matrix()
-            rnn_df.append(data_ if len(data_.shape) > 1 else [[i] for i in data_])
+    # X = np.zeros((nb_of_samples, sequence_len))
+    # for row_idx in range(nb_of_samples):
+    #     X[row_idx, :] = np.around(np.random.rand(sequence_len)).astype(int)
+    # # Create the targets for each sequence
+    # t = np.sum(X, axis=1)
+    
+    return X, t
 
-    return np.array(rnn_df, dtype=np.float32)
+def make_prediction(nb_of_samples):
+    sequence_len = 10
+    xs, ts = create_data(nb_of_samples, sequence_len)
+    return np.array([[[y] for y in x] for x in xs]), np.array([[x] for x in ts])
 
+def inference(input_ph, istate_ph):
+    with tf.name_scope("inference") as scope:
+        weight1_var = tf.Variable(tf.truncated_normal(
+            [num_of_input_nodes, num_of_hidden_nodes], stddev=0.1), name="weight1")
+        weight2_var = tf.Variable(tf.truncated_normal(
+            [num_of_hidden_nodes, num_of_output_nodes], stddev=0.1), name="weight2")
+        bias1_var = tf.Variable(tf.truncated_normal([num_of_hidden_nodes], stddev=0.1), name="bias1")
+        bias2_var = tf.Variable(tf.truncated_normal([num_of_output_nodes], stddev=0.1), name="bias2")
 
-def split_data(data, val_size=0.1, test_size=0.1):
-    """
-    splits data to training, validation and testing parts
-    """
-    ntest = int(round(len(data) * (1 - test_size)))
-    nval = int(round(len(data.iloc[:ntest]) * (1 - val_size)))
+        in1 = tf.transpose(input_ph, [1, 0, 2])
+        in2 = tf.reshape(in1, [-1, num_of_input_nodes])
+        in3 = tf.matmul(in2, weight1_var) + bias1_var
+        in4 = tf.split(in3, length_of_sequences)
 
-    df_train, df_val, df_test = data.iloc[:nval], data.iloc[nval:ntest], data.iloc[ntest:]
+        cell = tf.contrib.rnn.BasicLSTMCell(
+            num_of_hidden_nodes, forget_bias=forget_bias, state_is_tuple=False)
+        rnn_output, states_op = tf.contrib.rnn.static_rnn(cell, in4, initial_state=istate_ph)
+        output_op = tf.matmul(rnn_output[-1], weight2_var) + bias2_var
 
-    return df_train, df_val, df_test
-
-
-def prepare_data(data, time_steps, labels=False, val_size=0.1, test_size=0.1):
-    """
-    Given the number of `time_steps` and some data,
-    prepares training, validation and test data for an lstm cell.
-    """
-    df_train, df_val, df_test = split_data(data, val_size, test_size)
-    return (rnn_data(df_train, time_steps, labels=labels),
-            rnn_data(df_val, time_steps, labels=labels),
-            rnn_data(df_test, time_steps, labels=labels))
-
-def generate_data(fct, time_steps):
-    """generates data with based on a function fct"""
-    data = fct()
-    if not isinstance(data, pd.DataFrame):
-        data = pd.DataFrame(data)
-    train_x, val_x, test_x = prepare_data(data, time_steps)
-    train_y, val_y, test_y = prepare_data(data, time_steps, labels=True)
-    return dict(train=train_x, val=val_x, test=test_x), dict(train=train_y, val=val_y, test=test_y)
+        # Add summary ops to collect data
+        w1_hist = tf.summary.histogram("weights1", weight1_var)
+        w2_hist = tf.summary.histogram("weights2", weight2_var)
+        b1_hist = tf.summary.histogram("biases1", bias1_var)
+        b2_hist = tf.summary.histogram("biases2", bias2_var)
+        output_hist = tf.summary.histogram("output",  output_op)
+        results = [weight1_var, weight2_var, bias1_var,  bias2_var]
+        return output_op, states_op, results
 
 
-def lstm_model(num_units, rnn_layers, dense_layers=None, learning_rate=0.1, optimizer='Adagrad'):
-    """
-    Creates a deep model based on:
-        * stacked lstm cells
-        * an optional dense layers
-    :param num_units: the size of the cells.
-    :param rnn_layers: list of int or dict
-                         * list of int: the steps used to instantiate the `BasicLSTMCell` cell
-                         * list of dict: [{steps: int, keep_prob: int}, ...]
-    :param dense_layers: list of nodes for each layer
-    :return: the model definition
-    """
+def loss(output_op, supervisor_ph):
+    with tf.name_scope("loss") as scope:
+        square_error = tf.reduce_mean(tf.square(output_op - supervisor_ph))
+        loss_op = square_error
+        tf.summary.scalar("loss", loss_op)
+        return loss_op
 
-    def lstm_cells(layers):
-        if isinstance(layers[0], dict):
-            return [tf.nn.rnn.DropoutWrapper(tf.contrib.rnn.BasicLSTMCell(layer['num_units'], state_is_tuple=True), layer['keep_prob'])
-                    if layer.get('keep_prob') else tf.contrib.rnn.BasicLSTMCell(layer['num_units'], state_is_tuple=True) for layer in layers]
-        return [tf.contrib.rnn.BasicLSTMCell(steps, state_is_tuple=True) for steps in layers]
+def training(loss_op):
+    with tf.name_scope("training") as scope:
+        training_op = optimizer.minimize(loss_op)
+        return training_op
 
-    def dnn_layers(input_layers, layers):
-        if layers and isinstance(layers, dict):
-            return tflayers.stack(input_layers, tflayers.fully_connected,
-                                  layers['layers'],
-                                  activation=layers.get('activation'),
-                                  dropout=layers.get('dropout'))
-        elif layers:
-            return tflayers.stack(input_layers, tflayers.fully_connected, layers)
-        else:
-            return input_layers
+def calc_accuracy(output_op, prints=False):
+    inputs, ts = make_prediction(num_of_prediction_epochs)
+    pred_dict = {
+        input_ph:  inputs,
+        supervisor_ph: ts,
+        istate_ph:    np.zeros((num_of_prediction_epochs, num_of_hidden_nodes * 2)),
+    }
+    output = sess.run([output_op], feed_dict=pred_dict)
 
-    def _lstm_model(X, y):
-        stacked_lstm = tf.contrib.rnn.MultiRNNCell(lstm_cells(rnn_layers), state_is_tuple=True)
-        x_ = tf.unstack(X, axis=1, num=num_units)
-        output, layers = tf.contrib.rnn.static_rnn(stacked_lstm, x_, dtype=dtypes.float32)
-        output = dnn_layers(output[-1], dense_layers)
-        prediction, loss = tflearn.models.linear_regression(output, y)
-        train_op = tf.contrib.layers.optimize_loss(
-            loss, tf.contrib.framework.get_global_step(), optimizer=optimizer,
-            learning_rate=learning_rate)
-        return prediction, loss, train_op
+    def print_result(i, p, q):
+        for x in i:
+            print(list(x)[0]) 
+        print("output: %f, correct: %d" % (p, q))
+    if prints:
+        [print_result(i, p, q) for i, p, q in zip(inputs, output[0], ts)]
 
-    return _lstm_model
+    opt = abs(output - ts)[0]
+    total = sum([1 if x[0] < 0.05 else 0 for x in opt])
+    print("accuracy %f" % (total / float(len(ts))))
+    return output
 
-########
-# main #
-#######
-LOG_DIR = './ops_logs/exchange'
-TIMESTEPS = 3
-RNN_LAYERS = [{'num_units': 5}]
-DENSE_LAYERS = None
-TRAINING_STEPS = 10000
-PRINT_STEPS = TRAINING_STEPS / 10
-BATCH_SIZE = 100
+random.seed(0)
+np.random.seed(0)
+tf.set_random_seed(0)
 
-regressor = tflearn.Estimator(model_fn=lstm_model(TIMESTEPS, RNN_LAYERS, DENSE_LAYERS), model_dir=LOG_DIR)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
 
-X, y = generate_data(exchange_rate, TIMESTEPS)
+X, t = create_data(num_of_sample, length_of_sequences)
 
-# create a lstm instance and validation monitor
-validation_monitor = tflearn.monitors.ValidationMonitor(X['val'], y['val'], every_n_steps=PRINT_STEPS, early_stopping_rounds=1000)
-#print(X['train'])
-#print(y['train'])
+with tf.Graph().as_default():
+    input_ph = tf.placeholder(tf.float32, [None, length_of_sequences, num_of_input_nodes], name="input")
+    supervisor_ph = tf.placeholder(tf.float32, [None, num_of_output_nodes], name="supervisor")
+    istate_ph = tf.placeholder(tf.float32, [None, num_of_hidden_nodes * 2], name="istate")
 
-regressor.fit(X['train'], y['train'],
-                            monitors=[validation_monitor],
-                            batch_size=BATCH_SIZE,
-                            steps=TRAINING_STEPS)
+    output_op, states_op, datas_op = inference(input_ph, istate_ph)
+    loss_op = loss(output_op, supervisor_ph)
+    training_op = training(loss_op)
 
-predicted = regressor.predict(X['test'])
+    summary_op = tf.summary.merge_all()
+    init = tf.initialize_all_variables()
 
-ret_arr = []
-for val in predicted:
-    ret_arr.append(val)
+    with tf.Session() as sess:
+        saver = tf.train.Saver()
+        summary_writer = tf.summary.FileWriter("/tmp/tensorflow_log", graph=sess.graph)
+        sess.run(init)
 
-print(ret_arr)
+        for epoch in range(num_of_training_epochs):
+            inputs, supervisors = get_batch(size_of_mini_batch, X, t)
+            train_dict = {
+                input_ph:      inputs,
+                supervisor_ph: supervisors,
+                istate_ph:     np.zeros((size_of_mini_batch, num_of_hidden_nodes * 2)),
+            }
+            sess.run(training_op, feed_dict=train_dict)
 
-# rmse = np.sqrt(((predicted - y['test']) ** 2).mean(axis=0))
-# score = mean_squared_error(predicted, y['test'])
-# print ("MSE: %f" % score)
+            if (epoch) % 100 == 0:
+                summary_str, train_loss = sess.run([summary_op, loss_op], feed_dict=train_dict)
+                print("train#%d, train loss: %e" % (epoch, train_loss))
+                summary_writer.add_summary(summary_str, epoch)
+                if (epoch) % 500 == 0:
+                    calc_accuracy(output_op)
 
-# plot_predicted, = plt.plot(ret_arr, label='predicted')
-#plot_test, = plt.plot(y['test'], label='test')
-#plt.legend(handles=[plot_predicted, plot_test])
+        calc_accuracy(output_op, prints=True)
+        datas = sess.run(datas_op)
+        saver.save(sess, "model.ckpt")
