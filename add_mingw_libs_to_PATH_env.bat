dc54b137e8e6a157566622754d92d164f7d8140a
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-04-07 15:17:20 +0900

    refactord xgboost_trade_colab_gpu.py and worked at raptop cpu env.

---
+++ add_mingw_libs_to_PATH_env.bat
@@ -0,0 +1,1 @@
+set PATH=%PATH%;C:\msys64\mingw64\bin;C:\msys64\mingw64\bin;C:\msys64\mingw64\bin;C:\msys64\mingw64\usr\local\bin:C:\msys64\mingw64\usr\bin;C:\msys64\mingw64\bin;C:\msys64\mingw64\usr\bin\site_perl;C:\msys64\mingw64\usr\bin\vendor_perl;C:\msys64\mingw64\usr\bin\core_perl
