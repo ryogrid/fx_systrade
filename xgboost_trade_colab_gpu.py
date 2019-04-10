d1a15eefc1f8cb61a9409d4013af2f7ca535d9a1
Ryo Kanbayashi <ryo.contact@gmail.com>
2019-04-10 09:04:17 +0900

    disable stdout outputs on tarining and add logging tarining progress to text file.

--- xgboost_trade_colab_gpu.py
+++ xgboost_trade_colab_gpu.py
@@ -314,7 +314,7 @@ def train_and_generate_model():
     tr_input_arr = np.array(tr_input_mat)
     tr_angle_arr = np.array(tr_angle_mat)
     dtrain = xgb.DMatrix(tr_input_arr, label=tr_angle_arr)
-    param = {'max_depth':6, 'eta':0.2, 'subsumble':0.5, 'objective':'binary:logistic', 'verbosity':0, 'silent':False }
+    param = {'max_depth':6, 'eta':0.2, 'subsumble':0.5, 'objective':'binary:logistic', 'verbosity':0, 'silent':True }
     if is_use_gpu:
         param['tree_method'] = 'gpu_hist'
         param['max_bin'] = 16
