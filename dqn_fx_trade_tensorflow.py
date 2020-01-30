f69af895e4d50df7364f9f372facf756b83ccdcc
Ryo Kanbayashi <ryo.contact@gmail.com>
2020-01-30 10:57:25 +0900

    tarining running...

--- dqn_fx_trade_tensorflow.py
+++ dqn_fx_trade_tensorflow.py
@@ -17,6 +17,7 @@ import pickle
 from agent_fx_environment import FXEnvironment
 import os
 import sys
+import math
 
 # [1]損失関数の定義
 # 損失関数にhuber関数を使用 参考https://github.com/jaara/AI-blog/blob/master/CartPole-DQN.py
@@ -28,47 +29,61 @@ import sys
 #    loss = tf.where(cond, L2, L1)  # Keras does not cover where function in tensorflow :-(
 #    return K.mean(loss)
 
+reward_arr = np.array([])
+cur_idx_for_loss_calc = -1
+mu = 1.0
+sigma = 0.04
+SRATIO_PERIOD = 64
+
 def huberloss(y_true, y_pred):
     return tf.compat.v1.losses.huber_loss(y_true, y_pred)
+
+def sharpratio_loss(y, x):
+    global cur_idx_for_loss_calc
+
+    partial_reward_mean = np.mean(mu*(reward_arr[cur_idx_for_loss_calc - SRATIO_PERIOD:cur_idx_for_loss_calc]))
+    partial_reward_std = np.std(reward_arr[cur_idx_for_loss_calc - SRATIO_PERIOD:cur_idx_for_loss_calc])
+    cur_idx_for_loss_calc += 1
+    return partial_reward_std / partial_reward_mean
+
 
 # [2]Q関数をディープラーニングのネットワークをクラスとして定義
 class QNetwork:
     def __init__(self, learning_rate=0.001, state_size=15, action_size=3, hidden_size=10):
         self.model = Sequential()
         self.model.add(Dense(hidden_size, activation='relu', input_dim=state_size))
-        #self.model.add(Dense(hidden_size, activation='relu'))
-
-        #self.model.add(BatchNormalization()) -> バックテストしたらすごい勢いで減少した」
+        self.model.add(Dense(hidden_size, activation='relu'))
+        self.model.add(BatchNormalization())
         #self.model.add(Dropout(0.5))
 
-        self.model.add(Dense(action_size, activation='linear'))
+        self.model.add(Dense(action_size, activation='tanh'))
         self.optimizer = Adam(lr=learning_rate)  # 誤差を減らす学習方法はAdam
         #self.optimizer = Adam()  # 誤差を減らす学習方法はAdam. 学習係数はAdam optimizerのデフォルト値を使う.
         # self.model.compile(loss='mse', optimizer=self.optimizer)
         self.model.compile(loss=huberloss, optimizer=self.optimizer)
 
 
-    # 重みの学習
-    def replay(self, memory, batch_size, gamma):
-        inputs = np.zeros((batch_size, feature_num))
-        targets = np.zeros((batch_size, 3))
-        mini_batch = memory.sample(batch_size)
-
-        for i, (state_b, action_b, reward_b, next_state_b) in enumerate(mini_batch):
-            #inputs[i:i + 1] = state_b
-            inputs[i] = state_b
-            target = reward_b
-
-            retmainQs = self.model.predict(next_state_b)[0]
-            print(retmainQs)
-            next_action = np.argmax(retmainQs)  # 最大の報酬を返す行動を選択する
-            target = reward_b + gamma * retmainQs[next_action]
-
-
-            targets[i] = self.model.predict(state_b)    # Qネットワークの出力
-            targets[i][action_b] = target               # 教師信号
-
-        self.model.fit(inputs, targets, epochs=1, verbose=1)  # epochsは訓練データの反復回数、verbose=0は表示なしの設定
+    # # 重みの学習
+    # def replay(self, memory, batch_size, gamma):
+    #     inputs = np.zeros((batch_size, feature_num))
+    #     targets = np.zeros((batch_size, 3))
+    #     mini_batch = memory.sample(batch_size)
+    #
+    #     for i, (state_b, action_b, reward_b, next_state_b) in enumerate(mini_batch):
+    #         #inputs[i:i + 1] = state_b
+    #         inputs[i] = state_b
+    #         target = reward_b
+    #
+    #         retmainQs = self.model.predict(next_state_b)[0]
+    #         print(retmainQs)
+    #         next_action = np.argmax(retmainQs)  # 最大の報酬を返す行動を選択する
+    #         target = reward_b + gamma * retmainQs[next_action]
+    #
+    #
+    #         targets[i] = self.model.predict(state_b)    # Qネットワークの出力
+    #         targets[i][action_b] = target               # 教師信号
+    #
+    #     self.model.fit(inputs, targets, epochs=1, verbose=1)  # epochsは訓練データの反復回数、verbose=0は表示なしの設定
 
     # # 重みの学習
     # def replay(self, memory, batch_size, gamma):
@@ -107,6 +122,12 @@ class Memory:
         idx = np.random.choice(np.arange(len(self.buffer)), size=batch_size, replace=False)
         return [self.buffer[ii] for ii in idx]
 
+    def get_last(self, num):
+        deque_length = len(self.buffer)
+        start = deque_length - num
+        end = deque_length
+        return [self.buffer[ii] for ii in range(start, end)]
+
     def len(self):
         return len(self.buffer)
 
@@ -128,6 +149,7 @@ class Actor:
             retTargetQs = mainQN.model.predict(state)[0]
             #print(retTargetQs)
             action = np.argmax(retTargetQs)  # 最大の報酬を返す行動を選択する
+            action = round(action)
         else:
             action = np.random.choice([0, 1, 2])  # ランダムに行動する
 
@@ -142,18 +164,20 @@ gamma = 0.99  # 割引係数
 hidden_size = 50 #100 #50  # 16               # Q-networkの隠れ層のニューロンの数
 learning_rate = 0.005 #0.01 #0.001 #0.0001 # 0.00001         # Q-networkの学習係数
 memory_size = 7000000 #10000  # バッファーメモリの大きさ
-batch_size = 32  # Q-networkを更新するバッチの大きさ
+batch_size = 64 #32  # Q-networkを更新するバッチの大きさ
 num_episodes = TRAIN_DATA_NUM + 10  # envがdoneを返すはずなので念のため多めに設定 #1000  # 総試行回数
-iteration_num = 5 #25
+iteration_num = 160 #25
 feature_num = 13 #10 # 11
-
+nn_output_size = 1
 
 def tarin_agent():
+    global reward_arr
+
     env_master = FXEnvironment()
     islearned = 0  # 学習が終わったフラグ
 
     # [5.2]Qネットワークとメモリ、Actorの生成--------------------------------------------------------
-    mainQN = QNetwork(hidden_size=hidden_size, learning_rate=learning_rate, state_size=feature_num)     # メインのQネットワーク
+    mainQN = QNetwork(hidden_size=hidden_size, learning_rate=learning_rate, state_size=feature_num, action_size=nn_output_size)     # メインのQネットワーク
     # targetQN = QNetwork(hidden_size=hidden_size, learning_rate=learning_rate, state_size=feature_num)   # 価値を計算するQネットワーク
     # plot_model(mainQN.model, to_file='Qnetwork.png', show_shapes=True)        # Qネットワークの可視化
     memory = Memory(max_size=memory_size)
@@ -166,6 +190,10 @@ def tarin_agent():
         memory.load_memory("memory")
 
     total_get_acton_cnt = 1
+    do_fit_count = 0
+
+    inputs = np.zeros((batch_size, feature_num))
+    targets = np.zeros((batch_size, nn_output_size))
     for cur_itr in range(iteration_num):
         env = env_master.get_env('train')
         state, reward, done = env.step(0)  # 1step目は適当な行動をとる ("HOLD")
@@ -179,16 +207,33 @@ def tarin_agent():
             action = actor.get_action(state, total_get_acton_cnt, mainQN)  # 時刻tでの行動を決定する
             #action = actor.get_action(state, episode, mainQN)   # 時刻tでの行動を決定する
             next_state, reward, done = env.step(action)   # 行動a_tの実行による、s_{t+1}, _R{t}を計算する
-            #next_state = np.reshape(next_state, [1, 4])     # list型のstateを、1行4列の行列に変換
             next_state = np.reshape(state, [1, feature_num])  # list型のstateを、1行11列の行列に変換
+            np.insert(reward_arr, reward_arr.size, reward)
 
             memory.add((state, action, reward, next_state))     # メモリを更新する
             state = next_state  # 状態更新
 
-            # Qネットワークの重みを学習・更新する replay
-            if (memory.len() > batch_size) and not islearned:
-                mainQN.replay(memory, batch_size, gamma)
-                #mainQN.replay(memory, batch_size, gamma, targetQN)
+            do_fit_count+= 1
+            if do_fit_count % batch_size == 0 and do_fit_count != 0:
+                mini_batch = memory.get_last(batch_size)
+
+                for i, (state_b, action_b, reward_b, next_state_b) in enumerate(mini_batch):
+                    inputs[i] = state_b
+                    target = reward_b
+
+                    retmainQs = mainQN.model.predict(next_state_b)[0]
+                    print(retmainQs)
+                    #next_action = np.argmax(retmainQs)  # 最大の報酬を返す行動を選択する
+                    next_action_val = retmainQs[0]
+                    target = reward_b + gamma * next_action_val
+
+                    #targets[i] = mainQN.model.predict(state_b)  # Qネットワークの出力
+                    targets[i][0] = target  # 教師信号
+
+                cur_idx_for_loss_calc = episode
+                mainQN.model.fit(inputs, targets, epochs=1, verbose=1, batch_size=batch_size)  # epochsは訓練データの反復回数、verbose=0は表示なしの設定
+                inputs = np.zeros((batch_size, feature_num))
+                targets = np.zeros((batch_size, nn_output_size))
 
             # if DQN_MODE:
             #     # 行動決定と価値計算のQネットワークをおなじにする
