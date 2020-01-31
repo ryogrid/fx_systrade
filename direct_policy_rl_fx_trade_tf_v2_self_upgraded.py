0a2c9e365b490da5ec246a65485caff278d5b483
Ryo Kanbayashi <ryo.contact@gmail.com>
2020-01-31 19:09:42 +0900

    wip: learing running. but loss does not change. predicted value does not change...

--- direct_policy_rl_fx_trade_tf_v2_self_upgraded.py
+++ direct_policy_rl_fx_trade_tf_v2_self_upgraded.py
@@ -35,66 +35,23 @@ class MyModel(tf.keras.Model):
     def __init__(self, **kwargs):
         super(MyModel, self).__init__(**kwargs)
         #self.input_layer = layers.Input((10,))
-        self.hidden1 = layers.Dense(20, activation="relu")
-        self.hidden2 = layers.Dense(20, activation="relu")
-        self.output_dense = layers.Dense(3, activation="softmax")
+        self.hidden1 = layers.Dense(50, activation="relu", kernel_initializer='random_uniform', bias_initializer='zeros')
+        self.bnormalize1 = layers.BatchNormalization()
+        self.hidden2 = layers.Dense(50, activation="relu", kernel_initializer='random_uniform', bias_initializer='zeros')
+        self.dropout1 = layers.Dropout(0.5)
+        #self.output_dense = layers.Dense(1, activation="softmax")
+        self.output_dense = layers.Dense(1, activation="tanh")
 
     def __call__(self, inputs):
         #x = self.input_layer(inputs)
         x = self.hidden1(inputs)
+        x = self.bnormalize1(x)
         x = self.hidden2(x)
+        x = self.dropout1(x)
         x = self.output_dense(x)
         return x
 
-# class PolicyEstimator():
-#     def __init__(self):
-#         l_input = Input(shape=(NUM_STATES,))
-#         l_dense = Dense(20, activation='relu')(l_input)
-#         action_probs = Dense(NUM_ACTIONS, activation='softmax')(l_dense)
-#         #self.model = Model(inputs=[l_input], outputs=[action_probs])
-#         self.model = Model(inputs=l_input, outputs=action_probs)
-#         if os.path.exists("./policyEstimator_nw.json"):
-#             self.load_model("policyEstimator")
-#         self.state, self.action, self.target, self.action_probs, self.minimize, self.loss = self._build_graph(self.model)
-#
-#     def _build_graph(self, model):
-#
-#         state = tf.keras.backend.placeholder(dtype='float32', shape=(None, NUM_STATES))
-#         action = tf.keras.backend.placeholder(dtype='float32', shape=(None, NUM_ACTIONS))
-#         target = tf.keras.backend.placeholder(dtype='float32', shape=(None))
-#
-#         action_probs = model(state)
-#         log_prob = tf.math.log(tf.reduce_sum(input_tensor=action_probs * action))
-#         loss = lambda: -1 * log_prob * target
-#         print(log_prob)
-#         print(target)
-#
-#         #optimizer = tf.train.AdamOptimizer(LEARNING_RATE)
-#         optimizer = Adam(LEARNING_RATE)
-#         minimize = optimizer.minimize(loss, var_list=[log_prob, target])
-#
-#
-#         return state, action, target, action_probs, minimize, loss
-#
-#     def predict(self, sess, state):
-#         return sess.run(self.action_probs, {self.state: [state]})
-#
-#     def update(self, sess, state, action, target):
-#         feed_dict = {self.state: [state], self.target: target, self.action: to_categorical(action, NUM_ACTIONS)}
-#         _, loss = sess.run([self.minimize, self.loss], feed_dict)
-#         print("loss: " + str(loss))
-#         return loss
-#
-#     def save_model(self, file_path_prefix_str):
-#         with open("./" + file_path_prefix_str + "_nw.json", "w") as f:
-#             f.write(self.model.to_json())
-#         self.model.save_weights("./" + file_path_prefix_str + "_weights.hd5")
-#
-#     def load_model(self, file_path_prefix_str):
-#         with open("./" + file_path_prefix_str + "_nw.json", "r") as f:
-#             self.model = model_from_json(f.read())
-#         #self.model.compile(loss=huberloss, optimizer=self.optimizer)
-#         self.model.load_weights("./" + file_path_prefix_str + "_weights.hd5")
+
 
 MAXIMIZE_PERIOD = 64
 
@@ -106,19 +63,24 @@ def train(num_episodes, gamma=1.0):
     env_master = FXEnvironment()
 
     model = MyModel()
-    optim = Adam(learning_rate = 0.005)
+    optim = Adam(learning_rate = 0.0001)
 
-    def loss(pred, loss_arr, actions):
-        print(pred)
-        print(actions)
-        log_prob = tf.math.log(tf.reduce_sum(input_tensor=pred[0][actions[0]] * tf.cast(actions, np.float64)[0]))
-        return -1 * log_prob * loss_arr[0]
+    #def loss(pred, loss_arr, actions):
+    def loss(pred, loss_arr):
+        #print(pred)
+        #log_prob = tf.math.log(tf.reduce_sum(input_tensor=pred[0][actions[0]] * (tf.cast(actions, np.float64)[0]) + 100.0))
+        log_prob = tf.math.log(
+            tf.reduce_sum(input_tensor=pred[0][0] * 1.0 + 100.0))
+        #return -1 * log_prob * loss_arr[0]
+        return log_prob * loss_arr[0]
 
     @tf.function
-    def train_on_batch(X, loss_arr, actions):
+    def train_on_batch(X, loss_arr):
+    #def train_on_batch(X, loss_arr, actions):
         with tf.GradientTape() as tape:
             pred = model(X) # Train mode
-            loss_val = loss(pred, loss_arr, actions)
+            #loss_val = loss(pred, loss_arr, actions)
+            loss_val = loss(pred, loss_arr)
             # backward
             graidents = tape.gradient(loss_val, model.trainable_weights)
             # step optimizer
@@ -140,17 +102,10 @@ def train(num_episodes, gamma=1.0):
             input_state = np.reshape(np.array(state), [1, 10])
             #action_probs = model(input_state)[0]
             action_probs = model(input_state)[0].numpy()
-            #print(model(input_state))
-            print(action_probs)
-            # for ii, elem in enumerate(action_probs):
-            #     print(elem)
-            #     if elem < 0:
-            #         action_probs[ii] = 0
-            # sum = 0.0
-            # for ii, elem in enumerate(action_probs):
-            #     sum += elem
-
-            action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
+            action = round(action_probs[0] + 1)
+            #print(action)
+            print(action_probs[0])
+            #action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
             next_state, reward, done = env.step(action)
             rewards.append(reward)
 
@@ -170,14 +125,20 @@ def train(num_episodes, gamma=1.0):
                 input = np.reshape(np.array(state), [1, 10])
                 partial_return = np.mean(rewards[-1 * MAXIMIZE_PERIOD:])
                 std_on_partial = np.std(rewards[-1 * MAXIMIZE_PERIOD:])
-                target = partial_return / (std_on_partial + 0.00001) # sharp ration on MAXIMIZE_PERIOD
+                #target = partial_return / (std_on_partial + 0.00001) + 0.1 # sharp ration on MAXIMIZE_PERIOD
+                target = std_on_partial / (partial_return + 0.00001) + 2.0  # sharp ration on MAXIMIZE_PERIOD
+                #print(target)
                 if(math.isnan(target)):
                     target = 1.0
                     print("target is nan...")
                 calculated_loss_arr = np.array([target])
-                print(action)
-                actions = np.array([action])
-                loss_val = train_on_batch(input, calculated_loss_arr, actions)
+                # if action_count < 150000:
+                #     action = np.random.randint(0, 3)
+                # #print(action)
+                # actions = np.array([action])
+                # loss_val = train_on_batch(input, calculated_loss_arr, actions)
+                loss_val = train_on_batch(input, calculated_loss_arr)
+                print(loss_val)
                 #loss = policy_estimator.update(sess, step.state, step.action, target)
                 loss_list.append(loss)
 
@@ -212,17 +173,9 @@ def backtest(sess, policy_estimator, num_episodes, gamma=1.0):
         print('episode %s reward: %f last 100: %f' % (i_episode, total_reward))
 
 def train_agent():
-    # policy_estimator = PolicyEstimator()
-    # with tf.compat.v1.Session() as sess:
-    #     sess.run(tf.compat.v1.global_variables_initializer())
-    #     train(sess, policy_estimator, 100000, gamma=1)
     train(10000, gamma=1)
 
 def run_backtest():
-    #policy_estimator = PolicyEstimator()
-    #with tf.compat.v1.Session() as sess:
-    #    sess.run(tf.compat.v1.global_variables_initializer())
-    #    backtest(sess, policy_estimator, 1, gamma=1)
     backtest(1, gamma=1)
 
 if __name__ == '__main__':
