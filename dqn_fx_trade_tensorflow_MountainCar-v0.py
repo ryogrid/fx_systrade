8eaf3f6c16fc8a2b2558353e2336f8d0f3d639f0
Ryo Kanbayashi <ryo.contact@gmail.com>
2020-02-12 21:39:50 +0900

    wrote mountencar solver

--- dqn_fx_trade_tensorflow_MountainCar-v0.py
+++ dqn_fx_trade_tensorflow_MountainCar-v0.py
@@ -18,6 +18,7 @@ from agent_fx_environment import FXEnvironment
 import os
 import sys
 import math
+import gym
 
 # [1]損失関数の定義
 # 損失関数にhuber関数を使用 参考https://github.com/jaara/AI-blog/blob/master/CartPole-DQN.py
@@ -74,8 +75,7 @@ class QNetwork:
             targets[i] = self.model.predict(state_b)
             # BUYで暫定の rewardとして 0 を返されている場合は、それを用いて学習するとまずいので
             # predictした結果を採用させる（つまり、その場合以外であれば target を教師信号とする）
-            if not (action_b == 0 and reward_b == 0):
-                targets[i][action_b] = target  # 教師信号
+            targets[i][action_b] = target  # 教師信号
             #targets[i][2] = 0.0 + gamma * next_state_max_reward  # 教師信号（DONOTで返されるrewardは常に0)
 
         self.model.fit(inputs, targets, epochs=1, verbose=1)  # epochsは訓練データの反復回数、verbose=0は表示なしの設定
@@ -137,43 +137,35 @@ class Actor:
 
 # [5] メイン関数開始----------------------------------------------------
 # [5.1] 初期設定--------------------------------------------------------
-TRAIN_DATA_NUM = 223954 # 3years (test is 5 years)
 # ---
 gamma = 0.99 #0.3 #0.99  # 割引係数
-hidden_size = 50 # Q-networkの隠れ層のニューロンの数
+hidden_size = 20 # Q-networkの隠れ層のニューロンの数
 learning_rate = 0.001 #0.005 #0.01 # 0.05 #0.001 #0.0001 # 0.00001         # Q-networkの学習係数
 batch_size = 32 #64 # 32  # Q-networkを更新するバッチの大きさ
-num_episodes = TRAIN_DATA_NUM + 10  # envがdoneを返すはずなので念のため多めに設定 #1000  # 総試行回数
-iteration_num = 50 # <- 1足あたり 32 * 1 * 50 で約1500回のfitが行われる計算 #20
-memory_size = TRAIN_DATA_NUM * int(iteration_num * 0.1) #10000  # バッファーメモリの大きさ
-feature_num = 11 #10 #11
+num_episodes = 201 # envがdoneを返すはずなので念のため多めに設定 #1000  # 総試行回数
+iteration_num = 1000 # <- 1足あたり 32 * 1 * 50 で約1500回のfitが行われる計算 #20
+memory_size = num_episodes * int(iteration_num * 0.1) #10000  # バッファーメモリの大きさ
+feature_num = 2 #10 #11
 nn_output_size = 3
-TOTAL_ACTION_NUM = TRAIN_DATA_NUM * iteration_num
+TOTAL_ACTION_NUM = num_episodes * iteration_num
 
-def tarin_agent():
-    env_master = FXEnvironment()
+def train_agent():
+    env = gym.make('MountainCar-v0')
 
     # [5.2]Qネットワークとメモリ、Actorの生成--------------------------------------------------------
     mainQN = QNetwork(hidden_size=hidden_size, learning_rate=learning_rate, state_size=feature_num, action_size=nn_output_size)     # メインのQネットワーク
     targetQN = QNetwork(hidden_size=hidden_size, learning_rate=learning_rate, state_size=feature_num,
                       action_size=nn_output_size)  # 状態の価値を求めるためのネットワーク
     memory = Memory(max_size=memory_size)
-    memory_hash = {}
     actor = Actor()
-
-    if os.path.exists("./mainQN_nw.json"):
-        # 期間は最初からになってしまうが学習済みのモデルに追加で学習を行う
-        mainQN.load_model("mainQN")
-        targetQN.load_model("targetQN")
-        memory.load_memory("memory")
 
     total_get_acton_cnt = 1
 
     inputs = np.zeros((batch_size, feature_num))
     targets = np.zeros((batch_size, nn_output_size))
     for cur_itr in range(iteration_num):
-        env = env_master.get_env('train')
-        state, reward, done, info = env.step(2)  # 1step目は適当な行動をとる ("DONOT")
+        env.reset()
+        state, reward, done, info = env.step(env.action_space.sample())  # 1step目は適当な行動をとる ("DONOT")
         state = np.reshape(state, [1, feature_num])  # list型のstateを、1行15列の行列に変換
 
         # 状態の価値を求めるネットワークに、行動を求めるメインのネットワークの重みをコピーする（同じものにする）
@@ -181,20 +173,15 @@ def tarin_agent():
 
         for episode in range(num_episodes):  # 試行数分繰り返す
             total_get_acton_cnt += 1
-            action = actor.get_action(state, total_get_acton_cnt, mainQN)  # 時刻tでの行動を決定する
+            action = actor.get_action(state, cur_itr, mainQN)  # 時刻tでの行動を決定する
             next_state, reward, done, info = env.step(action)   # 行動a_tの実行による、s_{t+1}, _R{t}を計算する
-            next_state = np.reshape(state, [1, feature_num])  # list型のstateを、1行11列の行列に変換
+            next_state = np.reshape(next_state, [1, feature_num])  # list型のstateを、1行11列の行列に変換
 
-            # closeされた場合過去の各ポジションのopenについての獲得pipsが識別子文字列とともに
-            # info で返されるので、その内容で過去のエピソードのリワードを更新する
-            if len(info) > 1:
-                for keyval in info[1:]:
-                    memory_hash[keyval[0]][2] = keyval[1]
-
+            reward=0
+            if done:
+               reward=state[0]
             a_log = [state, action, reward, next_state]
             memory.add(a_log)     # メモリを更新する
-            # 後からrewardを更新するためにエピソード識別子をキーにエピソードを取得可能としておく
-            memory_hash[info[0]] = a_log
 
             state = next_state  # 状態更新
 
@@ -204,46 +191,8 @@ def tarin_agent():
 
             # 環境が提供する期間が最後までいった場合
             if done:
-                print('all training period learned.')
+                print(str(cur_itr)+":"+str(reward))
                 break
 
-            # モデルとメモリのスナップショットをとっておく
-            if episode % 10000 == 0 and episode != 0:
-                # targetQN.save_model("targetQN")
-                mainQN.save_model("mainQN")
-                memory.save_memory("memory")
-
-        # 一周回したら、次の周でりようされることはないのでクリア
-        memory_hash = {}
-
-def run_backtest():
-    env_master = FXEnvironment()
-    env = env_master.get_env("backtest")
-    num_episodes = TRAIN_DATA_NUM + 10 # envがdoneを返すはずなので念のため多めに設定 #1000  # 総試行回数
-
-    # [5.2]Qネットワークとメモリ、Actorの生成--------------------------------------------------------
-    mainQN = QNetwork(hidden_size=hidden_size, learning_rate=learning_rate)     # メインのQネットワーク
-    actor = Actor()
-
-    mainQN.load_model("mainQN")
-
-    # DONOT でスタート
-    state, reward, done, info = env.step(2)
-    state = np.reshape(state, [1, feature_num])
-    for episode in range(num_episodes):   # 試行数分繰り返す
-        action = actor.get_action(state, episode, mainQN, isBacktest = True)   # 時刻tでの行動を決定する
-        state, reward, done, info = env.step(action)   # 行動a_tの実行による、s_{t+1}, _R{t}を計算する
-        # 環境が提供する期間が最後までいった場合
-        if done:
-            print('all training period learned.')
-            break
-        state = np.reshape(state, [1, feature_num])
-
 if __name__ == '__main__':
-    np.random.seed(1337)  # for reproducibility
-    if sys.argv[1] == "train":
-        tarin_agent()
-    elif sys.argv[1] == "backtest":
-        run_backtest()
-    else:
-        print("please pass argument 'train' or 'backtest'")
+    train_agent()
