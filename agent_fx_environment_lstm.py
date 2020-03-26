edb00bd048983f670f33389ec6f82fad53227c52
Ryo Kanbayashi <ryo.contact@gmail.com>
2020-03-27 08:52:02 +0900

    index 28.

--- agent_fx_environment_lstm.py
+++ agent_fx_environment_lstm.py
@@ -23,7 +23,7 @@ class FXEnvironment:
         # self.SLIDE_IDX_NUM_AT_GEN_INPUTS_AND_COLLECT_LABELS = 1 #5
         self.PREDICT_FUTURE_LEGS = 5
         self.COMPETITION_DIV = True
-        self.COMPETITION_TRAIN_DATA_NUM =  72000 # <- 5分足で1年 #36000 # <- 10分足で1年 #72000 #36000 # 1000 # テスト期間でうまく動くまでは半年まで減らす #74651 # <- 検証中は期間を1年程度に減らす # 223954 # 3years (test is 5 years)
+        self.COMPETITION_TRAIN_DATA_NUM = 72000 # <- 5分足で1年 # 36000 # <- 10分足で1年 72000 #72000 #36000 # 1000 # テスト期間でうまく動くまでは半年まで減らす #74651 # <- 検証中は期間を1年程度に減らす # 223954 # 3years (test is 5 years)
 
         self.TRAINDATA_DIV = 2
         self.CHART_TYPE_JDG_LEN = 25
--- dqn_fx_trade_tensorflow_lstm.py
+++ dqn_fx_trade_tensorflow_lstm.py
@@ -16,7 +16,7 @@ if IS_TF_STYLE:
     from tensorflow.keras.models import Sequential, model_from_json, Model, load_model, save_model
     from tensorflow.keras.layers import Dense, BatchNormalization, Dropout, LSTM, RepeatVector, TimeDistributed, Reshape, LeakyReLU
     from tensorflow.keras.optimizers import Adam, SGD
-    from tensorflow.keras.regularizers import l2
+    from tensorflow.keras.regularizers import l1, l2
     #from tensorflow.keras.regularizers import l2
     #from keras import backend as K
 else:
@@ -58,11 +58,11 @@ class QNetwork:
                 ])
             else:
                 self.model = tf.keras.Sequential([
-                    LSTM(hidden_size, input_shape=(time_series, state_size), return_sequences=True), #activation=None), #recurrent_dropout=0.5),
-                    BatchNormalization(),
+                    LSTM(hidden_size, input_shape=(time_series, state_size), return_sequences=True, kernel_regularizer=l1(0.01), activation=None), #recurrent_dropout=0.5),
                     LeakyReLU(0.2),
+                    BatchNormalization(),
                     Dropout(0.5),
-                    LSTM(hidden_size, return_sequences=False), # activation=None), #recurrent_dropout=0.5),
+                    LSTM(hidden_size, return_sequences=False, kernel_regularizer=l1(0.01), activation=None), #recurrent_dropout=0.5),
                     LeakyReLU(0.2),
                     Dense(action_size, activation='linear')
                 ])
@@ -412,9 +412,9 @@ class Actor:
 #gamma = 0.95 # <- 今の実装では利用されていない #0.99 #0.3 # #0.99 #0.3 #0.99  # 割引係数
 hidden_size = 64 #32 #24 #50 #28 #80 #28 #50 # <- 50層だとバッチサイズ=32のepoch=1で1エピソード約3時間かかっていた # Q-networkの隠れ層のニューロンの数
 learning_rate = 0.0004 #0.0016 #0.0001 #0.01 #0.001 #0.01 #0.0005 # 0.0005 #0.0001 #0.005 #0.01 # 0.05 #0.001 #0.0001 # 0.00001         # Q-networkの学習係数
-time_series = 32 #64 #32 #64 #32
+time_series = 64 #32 #64 #32
 batch_size = 256 #1024 #64 #8 #64 #8 #1 #64 #16 #32 #16 #32 #64 # 32  # Q-networkを更新するバッチの大きさ
-TRAIN_DATA_NUM = 72000 - time_series # <- 5分足で1年 #36000 - time_series # <- 10分足で1年  #12000 - time_series # <- 30分足で1年 #72000 - time_series #36000 - time_series # 1000 - time_series #テストデータでうまくいくまで半年に減らす  #74651 # <- 検証中は期間を1年程度に減らす　223954 # 3years (test is 5 years)
+TRAIN_DATA_NUM = 72000 # <- 5分足で1年 # 36000 - time_series # <- 10分足で1年 #12000 - time_series # <- 30分足で1年 #72000 - time_series #36000 - time_series # 1000 - time_series #テストデータでうまくいくまで半年に減らす  #74651 # <- 検証中は期間を1年程度に減らす　223954 # 3years (test is 5 years)
 num_episodes = TRAIN_DATA_NUM + 10  # envがdoneを返すはずなので念のため多めに設定 #1000  # 総試行回数
 iteration_num = 5000 #720 # <- 劇的に減らす(1足あたり 16 * 1 * 50 で800回のfitが行われる計算) #720 #20
 #memory_size = TRAIN_DATA_NUM * iteration_num + 10 #TRAIN_DATA_NUM * int(iteration_num * 0.2) # 全体の20%は収まるサイズ. つまり終盤は最新の当該割合に対応するエピソードのみreplayする #10000
