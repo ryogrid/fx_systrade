10209d28a1f7cbcc7545968459e082257324fdc4
Ryo Kanbayashi <ryo.contact@gmail.com>
2020-04-02 04:40:37 +0900

    index 49. added tensorflow.set_random_sheed function call alfter model compile.

--- supervised_learning_dnn_agent_lstm.py
+++ supervised_learning_dnn_agent_lstm.py
@@ -48,6 +48,7 @@ class QNetwork:
         #self.model.compile(optimizer=self.optimizer, loss=self.loss_func)
         #self.model.compile(optimizer=self.optimizer, loss="sparse_categorical_crossentropy", metrics = ['accuracy'])
         self.model.compile(optimizer=self.optimizer, loss=self.loss_func, metrics=['accuracy'])
+        tf.set_random_seed(1337) # for reproductivity
         self.model.summary()
 
 
@@ -124,7 +125,7 @@ feature_num = 10
 nn_output_size = 2 #3
 HODABLE_POSITIONS = 100 #30
 predict_future_legs = 40
-epochs = 259 #4000 #45 #15 #45 # 90 #400
+epochs = 4000 #259 #4000 #45 #15 #45 # 90 #400
 half_spread = 0.0015
 
 BUY = 0
@@ -208,7 +209,7 @@ def limit_gpu_memory_usage():
             print(e)
 
 if __name__ == '__main__':
-    np.random.seed(1337)  # for reproducibility
+    #np.random.seed(1337)  # for reproducibility
 
     # バックテストだけ行う際はGPUで predictすると遅いので搭載されてないものとして動作させる
     if sys.argv[1] == "train":
