f61f95506ad7406cc532615031c0b284f37f186f
Ryo Kanbayashi <ryo.contact@gmail.com>
2020-05-29 09:06:26 +0900

    index147(148,149). EURJPY. learning period is head 6years and test period is rest years. unit num are 80 and 40. (2)

--- thesis_based_dqn_trade_agent.py
+++ thesis_based_dqn_trade_agent.py
@@ -297,7 +297,7 @@ time_series =  64 #32 #64 #USE_RECCURENT_LAYERがFalseでも利用されるの�
 if HALF_DAY_MODE:
     time_series = 2 * time_series
 batch_size = 64 #256 #1024
-TRAIN_DATA_NUM = 6 # 6years # 252 * 8 # 8years # 252 * 3 # 3years # 252 * 6 # 3years #  # 252 * 2 # 2years # 252 * 5 # 5year #72000
+TRAIN_DATA_NUM = 252 * 6 # 6years # 252 * 8 # 8years # 252 * 3 # 3years # 252 * 6 # 3years #  # 252 * 2 # 2years # 252 * 5 # 5year #72000
 if HALF_DAY_MODE:
     TRAIN_DATA_NUM = 2 * TRAIN_DATA_NUM
 num_episodes = TRAIN_DATA_NUM + 10  # envがdoneを返すはずなので念のため多めに設定
